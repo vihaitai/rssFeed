45733ebec707b058bebb0d2ee7e759b1a848136e
haitai <vihaitai@yahoo.com>
2020-03-12 16:40:34 +0800

    不同的源拆分到不同文件

--- seed/seed.go
+++ seed/seed.go
@@ -1,43 +1,11 @@
 package seed
 
-import (
-	"fmt"
-)
-
 type Seeder interface {
 	Platform() string
 	Link(int, int) string
 	Home() string
 	Name() string
 	Identifier() string
-}
-
-type Zhihu struct {
-	ID       string
-	Zhuanlan string
-}
-
-func (z *Zhihu) Link(offset, limit int) string {
-	if limit == 0 {
-		limit = 10
-	}
-	return fmt.Sprintf("https://zhuanlan.zhihu.com/api/columns/%s/articles?offset=%d&limit=%d", z.ID, offset, limit)
-}
-
-func (z *Zhihu) Home() string {
-	return fmt.Sprintf("https://zhuanlan.zhihu.com/%s", z.ID)
-}
-
-func (z *Zhihu) Name() string {
-	return z.Zhuanlan
-}
-
-func (z *Zhihu) Platform() string {
-	return "zhihu"
-}
-
-func (z *Zhihu) Identifier() string {
-	return z.ID
 }
 
 func NewSeeder(platform, id string, name string) Seeder {
---
+++ seed/zhihu.go
@@ -0,0 +1,31 @@
+package seed
+
+import "fmt"
+
+type Zhihu struct {
+	ID       string
+	Zhuanlan string
+}
+
+func (z *Zhihu) Link(offset, limit int) string {
+	if limit == 0 {
+		limit = 10
+	}
+	return fmt.Sprintf("https://zhuanlan.zhihu.com/api/columns/%s/articles?offset=%d&limit=%d", z.ID, offset, limit)
+}
+
+func (z *Zhihu) Home() string {
+	return fmt.Sprintf("https://zhuanlan.zhihu.com/%s", z.ID)
+}
+
+func (z *Zhihu) Name() string {
+	return z.Zhuanlan
+}
+
+func (z *Zhihu) Platform() string {
+	return "zhihu"
+}
+
+func (z *Zhihu) Identifier() string {
+	return z.ID
+}
